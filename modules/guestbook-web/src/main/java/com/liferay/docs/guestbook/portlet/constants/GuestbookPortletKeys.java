8437ee8c5817bf73c0936b7fafc40bfdb37404a6
manpakhong <manpakhong@192.168.61.14>
2018-05-23 16:29:46 +0800

    backend-initial - 20180523_1629

---
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author Administrator
+ */
+public class GuestbookPortletKeys {
+
+	public static final String Guestbook = "com_liferay_docs_guestbook_portlet_GuestbookPortlet";
+
+}
